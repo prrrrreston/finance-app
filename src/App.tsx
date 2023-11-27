be61eefee258589371a5e375cf6b1c7c347746a6
prrrrreston <122579382+prrrrreston@users.noreply.github.com>
2023-11-27 16:12:29 -0600

    integrated tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import './tailwind.css';
+
 
 function App() {
   return (
     <div>
-      <h1>welcome poop { new Date().toString() }</h1>
+      <h1 className= 'text-[100px] text-red-500'>welcome poop { new Date().toString() }</h1>
     </div>
   );
 }
