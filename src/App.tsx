d0a6cdb1de5d34e8e1a6f7975ce6ffbb0a1662bf
prrrrreston <122579382+prrrrreston@users.noreply.github.com>
2023-12-08 16:24:48 -0600

    added flexbox boilerplate to mainpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import './tailwind.css';
-import './App.scss';
 
 function App() {
   return (
-    <div className="mainContainer">
-      <div className='month'>
-        <h1>December</h1>
+    <div className="mainContainer flex flex-col items-center justify-around h-screen border border-red-500">
+      <div className='month flex border border-green-500'>December</div>
+      <div className="budgetContainer flex border border-yellow-500">
+        <div>Weekly budget</div>
+        <div>Monthly Budget</div>
       </div>
-      <div className="budgetContainer"></div>
-      <div className="logs"></div>
+      <div className="logs border border-blue-500">Log</div>
     </div>
   );
 }
